e8680aba54ed85968cfb6774e23b31e23075b25e
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2021-09-02 19:59:32 +0200

    added mail

--- src/components/view/ListView.jsx
+++ src/components/view/ListView.jsx
@@ -4,13 +4,21 @@ import "./ListView.css";
 
 const ListView = ({users}) =>{
 
+  const hideMail = (mail) => {
+    let result = mail.split("@");
+    result = `${result[0].slice(0,3)}...@${result[1]}`;
+    return  result;
+     }
+
+
+
     return(
   <ul className = "colection">
           {users.slice(0,10).map((post, index) =>(
             <li className="collection-item avatar" key = {index}>
                 <img src={post.picture.thumbnail} alt="" className="circle"/>
                 <span className="title">name: {post.name.first}</span>
-                <p>email: {post.email} <br/>
+                <p>email: {hideMail(post.email)} <br/>
                    date of birth: {post.dob.date}   
                 </p>
             </li> 
