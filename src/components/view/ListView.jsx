d3dd3958ad2e41b07e18f8b31488915bef876119
vladimir-799 <vladimir.bognar.dev@gmail.com>
2021-09-04 14:34:59 +0200

    added last name and search

--- src/components/view/ListView.jsx
+++ src/components/view/ListView.jsx
@@ -28,7 +28,7 @@ const ListView = ({users}) =>{
           {users.slice(0,10).map((post, index) =>(
             <li className="collection-item avatar" key = {index}>
                 <img src={post.picture.thumbnail} alt="" className="circle"/>
-                <span className="title">name: {capitalizeName(post.name.first)}</span>
+                <span className="title">name: {capitalizeName(post.name.first) + ' ' + post.name.last}</span>
                 <p>email: {hideMail(post.email)} <br/>
                    date of birth: {dateFormatter(post.dob.date)}   
                 </p>
