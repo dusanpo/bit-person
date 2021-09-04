0561a459b156ef311d67681a2a4f9e8a5deef801
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2021-09-04 14:27:58 +0200

    updated grid view and added buttton

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Header =  () => {
+const Header =  ({onLayoutToggle, isListView}) => {
+  const text = isListView ? <i className=" material-icons">view_module</i> : <i className="material-icons">view_list</i>;
 return(
 <nav>
 <div className="nav-wrapper">
 <label className="brand-logo">Bit Persons</label>
       <ul className="right hide-on-med-and-down">
+      <li onClick = {onLayoutToggle}>{text}</li>
         <li><a href="sass.html">Home</a></li>
         <li><i className="material-icons">refresh</i></li>
         <li><a href="collapsible.html">About</a></li>
